e9bb3d5e304d78f05c54b24e9fbbc06a29b17537
gwanghui97 <lgh970409@gmail.com>
2024-06-12 10:29:15 +0900

    feature Dto class

---
+++ src/main/java/com/springboot/coffee/NotZero.java
@@ -0,0 +1,20 @@
+package com.springboot.coffee;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {NotZeroValidator.class})
+public @interface NotZero {
+
+    int min() default Integer.MAX_VALUE;
+    int max() default Integer.MIN_VALUE;
+    String message() default "0 이외의 값이 들어와야 합니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
