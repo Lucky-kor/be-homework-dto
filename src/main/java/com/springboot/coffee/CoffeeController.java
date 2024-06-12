c170783cb8ac901d85a478a72be4c1af3ebe6a4e
yj <arcanacoach@gmail.com>
2024-06-12 10:33:49 +0900

    feature Dto Class

--- src/main/java/com/springboot/coffee/CoffeeController.java
+++ src/main/java/com/springboot/coffee/CoffeeController.java
@@ -2,43 +2,26 @@ package com.springboot.coffee;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
+@Validated
 @RequestMapping("/v1/coffees")
+
 public class CoffeeController {
     // 1. DTO 클래스 및 유효성 검증을 적용하세요.
     @PostMapping
-    public ResponseEntity postCoffee(@RequestParam("korName") String korName,
-                                     @RequestParam("engName") String engName,
-                                     @RequestParam("price") int price) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("korName", korName);
-        map.put("engName", engName);
-        map.put("price", price);
-
-
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    public ResponseEntity postCoffee(@Valid @RequestBody CoffeePostDto coffeePostDto) {
+        return new ResponseEntity<>(coffeePostDto, HttpStatus.CREATED);
     }
 
     // 2. DTO 클래스 및 유효성 검증을 적용하세요.
-    @PatchMapping("/{coffee-id}")
-    public ResponseEntity patchCoffee(@PathVariable("coffee-id") long coffeeId,
-                                      @RequestParam("korName") String korName,
-                                      @RequestParam("price") int price) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("coffeeId", coffeeId);
-        body.put("korName", korName);
-        body.put("engName", "Vanilla Latte");
-        body.put("price", price);
-        return new ResponseEntity<>(body, HttpStatus.OK);
-    }
-
-    @GetMapping("/{coffee-id}")
-    public ResponseEntity getCoffee(@PathVariable("coffee-id") long coffeeId) {
+    public ResponseEntity getCoffee(@Positive @PathVariable("coffee-id") long coffeeId) {
         System.out.println("# coffeeId: " + coffeeId);
 
         // not implementation
